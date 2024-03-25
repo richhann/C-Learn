45a14c29c04f7ca46e74d09dcb0f9c47721c20ae
Mingyou-Han <153143368+Mingyou-Han@users.noreply.github.com>
2024-03-25 13:56:22 +1100

    Printed Hello world and function call

---
+++ ProjectL2/ProjectL2/Log.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+
+void LOG(const char* message) {
+	std::cout << message << std::endl;
+}
+// C.out == character out, the c stands for "character"
--- ProjectL2/ProjectL2/Source.cpp
+++ ProjectL2/ProjectL2/Source.cpp
@@ -1,4 +1,20 @@
 #include <iostream> // io stands for input and output 
+// ^^^ that is a header file 
 
 using namespace std; //not sexually transmitted disease
+
+//Here is a declaration
+void LOG(const char* message);
+//This also works void LOG(const char*);
+
+/* Complier knows where to find the FUNCTION declartion bc LINKER*/
+
+int main() {
+	std::cout << "Hello World, This adds a new line" << std::endl;
+	std::cout << " Uses << -_- arrows... ";
+
+	LOG("THIS IS a message to our function!");
+
+	std::cin.get();
+}
 
